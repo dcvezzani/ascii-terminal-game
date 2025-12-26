62f4ff20795868d65530e4f9091d0ac41968fe5a
David Vezzani <dcvezzani@churchofjesuschrist.org>
2025-12-26 13:45:15 -0700

    Feature: Create game constants file
    - Created src/constants/gameConstants.js
    - Added EMPTY_SPACE_CHAR constant ('.')
    - Added WALL_CHAR constant ('#')
    - Added PLAYER_CHAR constant ('@')
    - Centralizes game character definitions
    - Used by Renderer for consistent character representation

---
+++ src/constants/gameConstants.js
@@ -0,0 +1,19 @@
+/**
+ * Game constants - centralized character and symbol definitions
+ */
+
+/**
+ * Character representing empty space on the board
+ */
+export const EMPTY_SPACE_CHAR = '.';
+
+/**
+ * Character representing a wall on the board
+ */
+export const WALL_CHAR = '#';
+
+/**
+ * Character representing the player
+ */
+export const PLAYER_CHAR = '@';
+
