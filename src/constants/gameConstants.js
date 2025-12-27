08a7fa55de7ffbda4c4921b76c3786feafe4bb82
David Vezzani <dcvezzani@churchofjesuschrist.org>
2025-12-26 17:27:44 -0700

    Refactor: Use Unicode escape for CP437 character 1 equivalent
    - Changed PLAYER_CHAR from literal ☺ to Unicode escape \u263A
    - Added comment explaining it's the CP437 char 1 equivalent
    - Makes it explicit that we're using the legacy character set equivalent
    - Unicode U+263A is the correct match for CP437 character 1 (smiley face)

--- src/constants/gameConstants.js
+++ src/constants/gameConstants.js
@@ -14,6 +14,8 @@ export const WALL_CHAR = '#';
 
 /**
  * Character representing the player
+ * Uses Unicode U+263A (WHITE SMILING FACE), which is the Unicode equivalent
+ * of Code Page 437 character 1 (the traditional ZZT smiley face)
  */
-export const PLAYER_CHAR = '@';
+export const PLAYER_CHAR = '\u263A'; // ☺ - CP437 char 1 equivalent
 
